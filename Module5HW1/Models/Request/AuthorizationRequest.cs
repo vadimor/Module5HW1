8abd01f36f738cb0fbad9c53ac2637a2eed15dbc
vadimor <vadimorbilyu@gmail.com>
2022-02-07 13:52:23 +0200

    Add request

---
+++ Module5HW1/Models/Request/AuthorizationRequest.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Module5HW1.Models.Request
+{
+    public class AuthorizationRequest
+    {
+        [JsonProperty("email")]
+        public string? Email { get; set; }
+
+        [JsonProperty("password")]
+        public string? Password { get; set; }
+    }
+}
---
+++ Module5HW1/Models/Request/WorkerRequest.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Module5HW1.Models.Request
+{
+    public class WorkerRequest
+    {
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+        [JsonProperty("job")]
+        public string? Job { get; set; }
+    }
+}
